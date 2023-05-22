5ca7422602e0ef2c45e2a3c63fed766d07ad5256
Dohyun Park <dhlife09@gmail.com>
2023-05-22 21:52:05 +0900

    update

---
+++ kw2030/script.js
@@ -0,0 +1,13 @@
+function getTimeHTML(distance) {
+
+    // 도보의 시속은 평균 4km/h 이고 도보의 분속은 67m/min입니다
+    var walkkTime = distance / 67 | 0;
+    var walkHour = '', walkMin = '';
+
+    // 계산한 도보 시간이 60분 보다 크면 시간으로 표시합니다
+    if (walkkTime > 60) {
+        walkHour = '<span class="number">' + Math.floor(walkkTime / 60) + '</span>시간 '
+    }
+    walkMin = '<span class="number">' + walkkTime % 60 + '</span>분'
+    return walkHour + walkMin;
+}
