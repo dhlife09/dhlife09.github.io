56262abc58bc127fba29c0f6a136102c7a9aa950
Dohyun Park <dhlife09@gmail.com>
2023-05-25 23:37:34 +0900

    Update rate_v1.3.js

--- kw2030/scripts/rate_v1.3.js
+++ kw2030/scripts/rate_v1.3.js
@@ -10,10 +10,10 @@ function rateRestaurant() {
         showCancelButton: true,
         focusConfirm: false,
         confirmButtonText:
-            '<i class="fa fa-thumbs-up"></i> Great!',
+            '<i class="fas fa-thumbs-up"></i> Great!',
         confirmButtonAriaLabel: 'Thumbs up, great!',
         cancelButtonText:
-            '<i class="fa fa-thumbs-down"></i>',
+            '<i class="fas fa-thumbs-down"></i>',
         cancelButtonAriaLabel: 'Thumbs down'
     })
 }
