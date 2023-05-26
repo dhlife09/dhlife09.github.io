48c3174644ddcd469a6e8e4d653d14a854ff9859
Dohyun Park <dhlife09@gmail.com>
2023-05-26 11:00:09 +0900

    Update marker_v1.3.js

--- kw2030/scripts/marker_v1.3.js
+++ kw2030/scripts/marker_v1.3.js
@@ -28,16 +28,20 @@ function createSpansWithTag(value) {    // 태그 생성 함수
 function newmarker(index, Lat, Lng, Title, imageUrl, Location, Time, Price, Menu, Promotion, Tag, SwalType, SupportTicket) {
     if (Promotion.includes("신규")) {
         var imageSrc = "https://dhlife09.github.io/kw2030/images/marker_green.png",
-            imageSize = new kakao.maps.Size(27, 40);
+            imageSize = new kakao.maps.Size(27, 40),
+            imageOption = {};
     } else if (SupportTicket) {
         var imageSrc = "https://dhlife09.github.io/kw2030/images/marker_purple.png",
-            imageSize   = new kakao.maps.Size(27, 40);
+            imageSize   = new kakao.maps.Size(27, 40),
+            imageOption = {};
     } else if (Promotion != "") {
         var imageSrc = "https://dhlife09.github.io/kw2030/images/marker_red.png",
-            imageSize   = new kakao.maps.Size(27, 40);
+            imageSize   = new kakao.maps.Size(27, 40),
+            imageOption = {};
     } else {
         var imageSrc = "https://dhlife09.github.io/kw2030/images/marker_blue.png",
-            imageSize = new kakao.maps.Size(27, 40);
+            imageSize = new kakao.maps.Size(27, 40),
+            imageOption = {};
     }
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
