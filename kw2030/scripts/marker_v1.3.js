7e9f144907000869e6f3d46b5a42730d698724dc
Dohyun Park <dhlife09@gmail.com>
2023-05-26 09:57:30 +0900

    Update marker_v1.3.js

--- kw2030/scripts/marker_v1.3.js
+++ kw2030/scripts/marker_v1.3.js
@@ -26,8 +26,8 @@ function createSpansWithTag(value) {    // 태그 생성 함수
  * @param {supportticket} SupportTicket 식권 지원 여부(bool) 
  */
 function newmarker(index, Lat, Lng, Title, imageUrl, Location, Time, Price, Menu, Promotion, Tag, SwalType, SupportTicket) {
-    var imageSrc = "https://dhlife09.github.io/kw2030/images/marker.png",
-        imageSize = new kakao.maps.Size(31, 35),
+    var imageSrc = "https://dhlife09.github.io/kw2030/images/marker_green.png",
+        imageSize = new kakao.maps.Size(27, 35),
         imageOption = {offset: new kakao.maps.Point(27, 69)};
         
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
