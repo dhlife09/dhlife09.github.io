df63cd668f2431690e0f9de73220c6452fe577a9
Dohyun Park <dhlife09@gmail.com>
2023-07-11 01:04:40 +0900

    test

--- busan-citytourbus/scripts/map_v1.3.js
+++ busan-citytourbus/scripts/map_v1.3.js
@@ -3,6 +3,9 @@
 kakaomap 렌더링
 
 */
+
+var mapWrapper = document.getElementById('mapWrapper'); //지도를 감싸고 있는 DIV태그
+
 var mapContainer = document.getElementById('map'),
     mapOption = {
         center: new kakao.maps.LatLng(35.1138657, 129.0399564),
@@ -24,30 +27,70 @@ var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 
-/*
-로드뷰
-*/
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW); //지도 위에 로드뷰 도로 올리기
 
-var roadviewContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
-var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
-var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
+var rvContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
+var rv = new kakao.maps.Roadview(rvContainer); //로드뷰 객체
+var rvClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
 
-var position = new kakao.maps.LatLng(35.1138657, 129.0399564);
+toggleRoadview(mapCenter);
 
-// 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
-roadviewClient.getNearestPanoId(position, 50, function(panoId) {
-    roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
+// 마커 이미지를 생성합니다.
+var markImage = new kakao.maps.MarkerImage(
+    'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/roadview_minimap_wk_2018.png',
+    new kakao.maps.Size(26, 46),
+    {
+        // 스프라이트 이미지를 사용합니다.
+        // 스프라이트 이미지 전체의 크기를 지정하고
+        spriteSize: new kakao.maps.Size(1666, 168),
+        // 사용하고 싶은 영역의 좌상단 좌표를 입력합니다.
+        // background-position으로 지정하는 값이며 부호는 반대입니다.
+        spriteOrigin: new kakao.maps.Point(705, 114),
+        offset: new kakao.maps.Point(13, 46)
+    }
+);
+
+// 드래그가 가능한 마커를 생성합니다.
+var rvMarker = new kakao.maps.Marker({
+    image : markImage,
+    position: mapCenter,
+    draggable: true,
+    map: map
 });
 
-function mapViewChanger() {
-    var mapDiv = document.getElementById("map");
-    var roadviewDiv = document.getElementById("roadview");
-  
-    if (mapDiv.style.display === "none") {
-      mapDiv.style.display = "block";
-      roadviewDiv.style.display = "none";
-    } else {
-      mapDiv.style.display = "none";
-      roadviewDiv.style.display = "block";
-    }
-  }
+//마커에 dragend 이벤트를 할당합니다
+kakao.maps.event.addListener(rvMarker, 'dragend', function(mouseEvent) {
+    var position = rvMarker.getPosition(); //현재 마커가 놓인 자리의 좌표
+    toggleRoadview(position); //로드뷰를 토글합니다
+});
+
+//지도에 클릭 이벤트를 할당합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent){
+    
+    // 현재 클릭한 부분의 좌표를 리턴 
+    var position = mouseEvent.latLng; 
+
+    rvMarker.setPosition(position);
+    toggleRoadview(position); //로드뷰를 토글합니다
+});
+
+//로드뷰 toggle함수
+function toggleRoadview(position){
+
+    //전달받은 좌표(position)에 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄웁니다
+    rvClient.getNearestPanoId(position, 50, function(panoId) {
+        if (panoId === null) {
+            rvContainer.style.display = 'none'; //로드뷰를 넣은 컨테이너를 숨깁니다
+            mapWrapper.style.width = '100%';
+            map.relayout();
+        } else {
+            mapWrapper.style.width = '50%';
+            map.relayout(); //지도를 감싸고 있는 영역이 변경됨에 따라, 지도를 재배열합니다
+            rvContainer.style.display = 'block'; //로드뷰를 넣은 컨테이너를 보이게합니다
+            rv.setPanoId(panoId, position); //panoId를 통한 로드뷰 실행
+            rv.relayout(); //로드뷰를 감싸고 있는 영역이 변경됨에 따라, 로드뷰를 재배열합니다
+        }
+    });
+}
