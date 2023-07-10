d52cdf8961db1d173cddcb94c151f948ada5cc32
Dohyun Park <dhlife09@gmail.com>
2023-07-11 02:03:01 +0900

    Update items_v1.4.js

--- busan-citytourbus/scripts/items_v1.4.js
+++ busan-citytourbus/scripts/items_v1.4.js
@@ -1,19 +1,3 @@
-/*
-newmarker(0, 37.6197915, 127.0613631, "후문식당", "backdoor.jpg", "광운대후문 FM25.1 카페 오른쪽", "월~토 10:00~19:00", "5,000", "https://dhlife09.github.io/kw2030/images/backdoor-menu.png", "", "#한식 #가격만족도최고❤️", "info", false, 7);
-newmarker(1, 37.6205277, 127.0585612, "김밥천국 광운대점", "kimcheon.jpg", "광운대학교 앞", "매일 09:00~21:00", "6,000", "https://dhlife09.github.io/kw2030/images/kimcheon-menu.png", "[해피아워 이벤트] 14시~16시에 이용하시는 고객에게 서비스 음료 제공", "#분식 #혼밥", "info", false, 6);
-newmarker(2, 37.6212411, 127.0597654, "밥은화 광운대점", "riceenfire.jpg", "삼거리에서 오른쪽", "월~토 11:00~20:00", "6,000", "https://pcmap.place.naver.com/restaurant/793852255/menu", "위 제휴 QR코드를 보여주시면 음료수를 서비스로 드려요!", "#한식 #덮밥 #친절😊", "image", false, 4);
-newmarker(3, 37.6192242, 127.0583769, "함지마루(학생식당)", "hamzee.jpg", "광운대학교 복지관 2층", "평일 11:30~14:00", "5,500", "https://www.kw.ac.kr/ko/life/facility11.jsp", "※ 1,000원의 아침밥🍚을 만나보세요!<br>1. 기간 : 2023.05.02.(화) ~11.30.(목) (* 휴일 및 방학 기간 제외)<br>2. 시간 : 평일 8:30 ~ 9:30<br><br>※ PAYCO 포인트카드로 결제 시 결제금액의 10% 를 적립해드려요!", "#한식 #넓은 #혼밥", "info", true, 1);
-newAdvertisement(0, "ad-kosaf-logo.jpg", "2차 국가장학금 신청하세요!", "5월 23일부터 6월 22일까지", "https://www.kosaf.go.kr");
-newmarker(4, 37.6200573, 127.0586526, "카츠3.3", "katsu.jpg", "광운대 80주년기념관 1층 푸드코트", "매일 10:00~21:00", "5,900", "https://pcmap.place.naver.com/restaurant/859267666/menu", "※ 위 제휴 QR코드를 보여주시면 광운대학교 학생에게 공기(밥)을 무료로 제공합니다!", "#일식 #카츠 #라멘 #가성비", "image", false, 6);
-newmarker(5, 37.6203015, 127.0584836, "마루", "maru.jpg", "광운대학교 앞", "매일 11:00~21:30", "5,000", "https://dhlife09.github.io/kw2030/images/maru-menu.png", "", "#컵밥 #가성비 #혼밥", "info", false, 4);
-newmarker(6, 37.6197503, 127.060937, "새빛식당", "saebit.jpg", "광운대학교 새빛관 13층 푸드코트", "평일 오전11시~오후8시", "5,000", "https://dhlife09.github.com/kw2030/images/noimage.png", "[신규 오픈 및 해피아워 이벤트] 여기요 제휴 QR코드를 보여주시면 10%할인! 2시~4시에 이용하시는 고객에게 음료수 무료제공!", "#가성비 #분식", "image", true, 8);
-newmarker(7, 37.6186687, 127.0572381, "미식성", "misik.jpg", "광운대학교 연구관 옆으로 쭉", "매일 10:00~21:00", "7,000", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#중식 #단체모임 #넓은", "info", false, 7);
-newAdvertisement(1, "seoul-logo.jpg", "서울 청년 교통비🚌지원", "1998~2004년생 청년에게 최대 10만원", "https://seoul.go.kr");
-newmarker(8, 37.619348, 127.0574993, "김국수", "kimguksu.jpg", "광운대학교 연구관 옆 건물 2층", "평일 오후12시~오후8시", "6,000", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#김밥 #국수 #가격만족도높음❤️", "info", true, 10);
-newmarker(9, 37.6212654, 127.0598852, "꽃제비칼국수", "noodle.jpg", "삼거리에서 오른쪽 밥은화옆", "평일 오후12시~오후8시", "6,000", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#국수 #서비스", "info", false, 8);
-newmarker(10, 37.6214627, 127.0590256, "노원설렁탕", "sull.jpg", "광운대학교 연구관 옆 건물 2층", "평일 오후12시~오후8시", "4,000", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#분식 #친절❤️ #반찬잘나옴 #집밥", "info", false, 6);
-newAdvertisement(2, "kw-logo.jpg", "2학기 참빛설계학기 참가팀 모집", "내가 설계한 프로젝트하고 학점받자!", "https://www.kw.ac.kr/ko/life/notice.jsp?BoardMode=view&DUID=42702&tpage=1&searchKey=1&searchVal=%EC%B0%B8%EB%B9%9B&srCategoryId=");
-*/
 newmarker(0, 35.1138657, 129.0399564 , "부산역(출발)", "LINE_RED.png", "출발", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#레드라인", "info", false, 0);
 newmarker(1, 35.1272753, 129.0953989, "UN기념공원", "LINE_RED.png", "x", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#레드라인", "info", false, 0);
 newmarker(2, 35.1304939, 129.0929988, "부산박물관", "LINE_RED.png", "x", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#레드라인", "info", false, 0);
@@ -27,40 +11,43 @@ newmarker(9, 35.1673957, 129.1369395, "시립미술관.벡스코", "LINE_RED.png
 newmarker(10, 35.1269429, 129.1015538, "평화공원", "LINE_RED.png", "x", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#레드라인", "info", false, 0);
 newmarker(11, 35.0999311, 129.0370273, "광복로", "LINE_RED.png", "x", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#레드라인", "info", false, 0);
 newmarker(12, 35.1138657, 129.0399564 , "부산역(도착)", "LINE_RED.png", "도착", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#레드라인", "info", false, 0);
-newmarker(13, 35.1632355, 128.984391, "부산서부(사상)터미널", "noimage.png", "터미널", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "터미널", "info", false, 0);
-newmarker(14, 35.1010192, 129.0339937, "엘리제호텔", "noimage.png", "호텔", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "호텔", "info", false, 0);
-newmarker(15, 35.1138657, 129.0399564, "부산역(출발)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(16, 35.0968692, 129.0359784, "영도대교(유라리광장)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(17, 35.0807121, 129.0443038, "흰여울 문화마을", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(18, 35.0722344, 129.0552553, "하늘전망대", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(19, 35.0689159, 129.0654513, "영도해녀촌(노을전망대)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(20, 35.0621083, 129.0814542, "태종대", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(21, 35.0787642, 129.0784291, "국립해양박물관", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(22, 35.1029086, 129.1192664, "오륙도스카이워크", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(23, 35.1317634, 129.1134165, "용호만유람선터미널", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(24, 35.1269429, 129.1015538, "평화공원", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(25, 35.077152, 129.0214538, "송도해수욕장(구름산책로)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(26, 35.0972807, 129.0291291, "자갈치,BIFF광장(용두산공원)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(27, 35.1138657, 129.0399564, "부산역(도착)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
-newmarker(28, 35.1138657, 129.0399564, "부산역(출발)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(29, 35.076461, 129.0226323, "송도해수욕장(송도구름산책로)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(30, 35.0629603, 129.019136, "암남공원(용궁구름다리)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(31, 35.0879154, 129.0040281, "감천문화마을(감천사거리)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(32, 35.04869, 128.9684473, "다대포해수욕장(몰운대)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(33, 35.0526593, 128.9606945, "아미산전망대", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(34, 35.078505, 128.9515674, "부네치아 장림항", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(35, 35.1079622, 128.9449864, "부산현대미술관(을숙도)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(36, 35.1079622, 128.9449864, "(휴업중)낙동강하구 에코센타", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(37, 35.1031514, 129.0194478, "석당박물관(임시수도기념관)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(38, 35.1006752, 129.0257413, "국제시장(보수동책방골목)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(39, 35.1030982, 129.0327819, "용두산공원(부산근대역사관)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-newmarker(40, 35.1138657, 129.0399564, "부산역(도착)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
-// 35.2100114!4d126.8458791 집결지(7시40분까지 1101동)
-newmarker(41, 35.2100114, 126.8458791, "집결지(7시40분까지 1101동)", "noimage.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "광주", "info", false, 0);
-// 35.1603087!4d126.8801307 광주종합버스터미널
-newmarker(42, 35.1603087, 126.8801307, "광주종합버스터미널", "noimage.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "광주", "info", false, 0);
-newmarker(43, 35.1138657, 129.0399564, "부산역(환승)", "LINE_PRIMARY.png", "레드라인, 오렌지라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
-newmarker(44, 35.076461, 129.0226323, "송도해수욕장(송도구름산책로)(환승)", "LINE_PRIMARY.png", "오렌지라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
-newmarker(45, 35.1317634, 129.1134165, "용호만유람선터미널(환승)", "LINE_PRIMARY.png", "레드라인, 블루라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
-newmarker(46, 35.1595823, 129.161566, "해운대해수욕장(환승)", "LINE_PRIMARY.png", "레드라인 블루라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
-newmarker(47, 35.1269429, 129.1015538, "평화공원(환승)", "LINE_PRIMARY.png", "레드라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
+
+newmarker(13, 35.1138657, 129.0399564, "부산역(출발)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(14, 35.0968692, 129.0359784, "영도대교(유라리광장)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(15, 35.0807121, 129.0443038, "흰여울 문화마을", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(16, 35.0722344, 129.0552553, "하늘전망대", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(17, 35.0689159, 129.0654513, "영도해녀촌(노을전망대)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(18, 35.0621083, 129.0814542, "태종대", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(19, 35.0787642, 129.0784291, "국립해양박물관", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(20, 35.1029086, 129.1192664, "오륙도스카이워크", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(21, 35.1317634, 129.1134165, "용호만유람선터미널", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(22, 35.1269429, 129.1015538, "평화공원", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(23, 35.077152, 129.0214538, "송도해수욕장(구름산책로)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(24, 35.0972807, 129.0291291, "자갈치,BIFF광장(용두산공원)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+newmarker(25, 35.1138657, 129.0399564, "부산역(도착)", "LINE_GREEN.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#그린라인", "info", false, 0);
+
+newmarker(26, 35.1138657, 129.0399564, "부산역(출발)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(27, 35.076461, 129.0226323, "송도해수욕장(송도구름산책로)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(28, 35.0629603, 129.019136, "암남공원(용궁구름다리)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(29, 35.0879154, 129.0040281, "감천문화마을(감천사거리)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(30, 35.04869, 128.9684473, "다대포해수욕장(몰운대)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(31, 35.0526593, 128.9606945, "아미산전망대", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(32, 35.078505, 128.9515674, "부네치아 장림항", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(33, 35.1079622, 128.9449864, "부산현대미술관(을숙도)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(34, 35.1079622, 128.9449864, "(휴업중)낙동강하구 에코센타", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(35, 35.1031514, 129.0194478, "석당박물관(임시수도기념관)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(36, 35.1006752, 129.0257413, "국제시장(보수동책방골목)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(37, 35.1030982, 129.0327819, "용두산공원(부산근대역사관)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+newmarker(38, 35.1138657, 129.0399564, "부산역(도착)", "LINE_ORANGE.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#오렌지라인", "info", false, 0);
+
+newmarker(39, 35.1138657, 129.0399564, "부산역(환승)", "LINE_PRIMARY.png", "레드라인, 오렌지라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
+newmarker(40, 35.076461, 129.0226323, "송도해수욕장(송도구름산책로)(환승)", "LINE_PRIMARY.png", "오렌지라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
+newmarker(41, 35.1317634, 129.1134165, "용호만유람선터미널(환승)", "LINE_PRIMARY.png", "레드라인, 블루라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
+newmarker(42, 35.1595823, 129.161566, "해운대해수욕장(환승)", "LINE_PRIMARY.png", "레드라인 블루라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
+newmarker(43, 35.1269429, 129.1015538, "평화공원(환승)", "LINE_PRIMARY.png", "레드라인, 그린라인 환승장", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "#환승", "info", false, 0);
+
+newmarker(44, 35.2100114, 126.8458791, "집결지(7시40분까지 1101동)", "noimage.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "광주", "info", false, 0);
+newmarker(45, 35.1603087, 126.8801307, "광주종합버스터미널", "noimage.png", "-", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "광주", "info", false, 0);
+
+newmarker(46, 35.1632355, 128.984391, "부산서부(사상)터미널", "noimage.png", "터미널", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "터미널", "info", false, 0);
+newmarker(47, 35.1010192, 129.0339937, "엘리제호텔", "noimage.png", "호텔", "", "0", "https://dhlife09.github.io/kw2030/images/noimage.png", "", "호텔", "info", false, 0);
